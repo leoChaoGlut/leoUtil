167ce90ad67f8299479c9d2cef12d0f5f9e0286e
leoChaoGlut <121559258@qq.com>
2016-03-15 23:50:13 +0800

    init

---
+++ src/restful/Response.java
@@ -0,0 +1,33 @@
+package restful;
+/**
+ * 
+ * @author "leo"
+ * @Description resful 返回值
+ * @date 2016年3月15日 下午9:49:58
+ * @version v0.0.1
+ */
+public class Response {
+
+	private int status;
+	private String msg;
+	private String result;
+
+	public Response() {
+	}
+
+	public Response(int status, String msg, String result) {
+		this.status = status;
+		this.msg = msg;
+		this.result = result;
+	}
+
+	public static Response success(String jsonMsg) {
+		return new Response(Status.OK.getCode(), Status.OK.getMsg(), jsonMsg);
+	}
+
+	public static Response error() {
+		// TODO 未完成
+		return null;
+	}
+
+}
---
+++ src/restful/Status.java
@@ -0,0 +1,25 @@
+package restful;
+
+public enum Status {
+
+	OK(200, "成功"), NOT_MODIFIED(304, "文件未修改"), BAD_REQUEST(400, "错误请求"), UNAUTHORIZED(
+			401, "未授权"), FORBIDDEN(403, "请求被拒绝"), NOT_FOUND(404, "请求的页面不存在"), INTERNAM_SERVER_ERROR(
+			500, "服务器内部错误");
+
+	private int code;
+	private String msg;
+
+	private Status(int code, String msg) {
+		this.code = code;
+		this.msg = msg;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+}
